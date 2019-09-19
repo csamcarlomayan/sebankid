c73ab091a779a8d29b1414b69968e712491ad873
Carl Omayan <carl.omayan@csamhealth.com>
2019-09-19 15:49:43 +0800

    test

--- src/android/sebankid.java
+++ src/android/sebankid.java
@@ -8,17 +8,23 @@ import android.net.Uri;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaInterface;
+import android.content.Context;
+import android.content.Intent;
 /**
  * This class echoes a string called from JavaScript.
  */
 public class sebankid extends CordovaPlugin {
-
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Context context = cordova.getActivity().getApplicationContext();
         if (action.equals("startAuthCall")) {
             String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.coolMethod(message, context , callbackContext);
             return true;
         }
         return false;
@@ -36,12 +42,12 @@ public class sebankid extends CordovaPlugin {
         startActivity(intent); */
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        Intent intent = new Intent();
+    private void coolMethod(String message, Context context, CallbackContext callbackContext) {
+        Intent intent = new Intent(context);
         intent.setPackage("com.bankid.bus");
         intent.setAction(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("bankid://autostarttoken=" + starttoken + "&redirect=null "));
-        callbackContext.startActivity(intent);
+        context.startActivity(intent);
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
