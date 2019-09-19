1fc8d85bdd01366b8d67e6abd267945aa5ee5a65
Carl Omayan <carl.omayan@csamhealth.com>
2019-09-19 13:51:00 +0800

    test

--- src/android/sebankid.java
+++ src/android/sebankid.java
@@ -18,7 +18,7 @@ public class sebankid extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.coolMethod(message + "FROM JAVA CODE", callbackContext);
             return true;
         }
         return false;
