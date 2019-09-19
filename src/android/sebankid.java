c725fb552784af97e5215d42afeeea33bff695d9
Carl Omayan <carl.omayan@csamhealth.com>
2019-09-19 13:14:13 +0800

    test update on token

--- src/android/sebankid.java
+++ src/android/sebankid.java
@@ -24,6 +24,11 @@ public class sebankid extends CordovaPlugin {
         return false;
     }
     public void startAuthCall(String starttoken) {
+        if (starttoken != null && starttoken.length() > 0) {
+            callbackContext.success(starttoken);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
         Intent intent = new Intent();
         intent.setPackage("com.bankid.bus");
         intent.setAction(Intent.ACTION_VIEW);
